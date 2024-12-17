acd9c20ffa066a956683faac3f05b1275a05f9ef
julien Moulis <julien.moulis@moulis.me>
2024-12-17 15:57:32 +0100

    Start Demo

---
+++ demo/script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+  /** valeur primitive
+   * numeric
+   * string
+   * array
+   * object
+   * boolean
+   *
+   *
+   *
+   */
+  //
+  /** variable
+   *  const
+   *  let
+   */
+  // function
+
+  const age = 20;
+  const ageFois2 = age * 2;
+
+  const prenom = 'Jean';
+  const lastname = 'Dupont';
+  const fullname = `${prenom} ${lastname} a ${ageFois2}ans`;
+
+  console.log(ageFois2);
+  console.log(prenom);
+  console.log(lastname);
+  console.log(fullname);
+});
